7bbac6be44e1b914c1092d7e353941878614d45b
nathan <bronifty@gmail.com>
2022-12-21 00:36:59 -0500

    .

--- src/sqlite-test.js
+++ src/sqlite-test.js
@@ -39,24 +39,3 @@ class Database {
 
 const database = new Database();
 export default database;
-
-// export async function getUser(username) {
-//   const [rows] = await connection.promise().query(
-//     `SELECT *
-//       FROM users
-//       WHERE username = ?`,
-//     [username]
-//   );
-
-//   return rows[0];
-// }
-
-// export async function createUser(username, password) {
-//   const { insertId } = await connection.promise().query(
-//     `INSERT INTO users (username, password)
-//       VALUES (?, ?)`,
-//     [username, password]
-//   );
-
-//   return insertId;
-// }
